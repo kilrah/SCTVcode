e178964e6918741b0c7e21586e0beaab9b36b0c3
amalmin <amalmin@okb-1.org>
2021-12-30 22:56:03 -0800

    starfield tweaks

--- SCTVcode/u_stars.ino
+++ SCTVcode/u_stars.ino
@@ -1,4 +1,4 @@
-// starfield from https://github.com/jordanmack/arduino-starfield
+// original starfield code from https://github.com/jordanmack/arduino-starfield
 // Hacked up by Audin Malmin.
 
 const int starCount = 512; // number of stars in the star field
@@ -11,8 +11,8 @@ void reset_stars() {
   for (int i = 0; i < starCount; i++) {
     stars[i][0] = random(-25, 25);
     stars[i][1] = random(-25, 25);
-    stars[i][2] = random(0, maxDepth);
-    stars[i][3] = random(5)+1;
+    stars[i][2] = random(0, maxDepth);   // distance
+    stars[i][3] = random(5)+1;           // "absolute magnitude"
   }
 }
 
@@ -21,11 +21,10 @@ void doStars() {
   int origin_y = 1500;
   int screen_size = 1500*2;
 
-  // Iterate through the stars reducing the z co-ordinate in order to move the
-  // star closer.
+  // Iterate through the stars reducing the z co-ordinate in order to move the star closer.
   for (int i = 0; i < starCount; ++i) {
-    //       stars[i][2] -= 0.19;
     stars[i][2] -= 0.29;
+
     // if the star has moved past the screen (z < 0) reposition it far away
     // with random x and y positions.
     if (stars[i][2] <= 0) {
@@ -37,13 +36,12 @@ void doStars() {
 
     // Convert the 3D coordinates to 2D using perspective projection.
     double k = screen_size / stars[i][2];
-    int x = static_cast<int>(stars[i][0] * k + origin_x);
-    int y = static_cast<int>(stars[i][1] * k + origin_y);
+    int x = stars[i][0] * k + origin_x;
+    int y = stars[i][1] * k + origin_y;
 
     // Draw the star (if it is visible in the screen).
     // Distant stars are smaller than closer stars.
-    if ((0 <= x and x < screen_size)
-        and (0 <= y and y < screen_size)) {
+    if ((0 <= x && x < screen_size) && (0 <= y && y < screen_size)) {
       int size = (1 - stars[i][2] / maxDepth) * 8;
 
 //      if (i == 1)
