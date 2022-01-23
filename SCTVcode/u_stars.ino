77c270425622200ff3d5130db72d7101b9db758c
amalmin <amalmin@okb-1.org>
2022-01-23 13:39:52 -0800

    Stars increase magnitude as they move closer.  The whole starfield now fades into view when the face is started.

--- SCTVcode/u_stars.ino
+++ SCTVcode/u_stars.ino
@@ -11,7 +11,7 @@ const int maxDepth = 72;   // maximum distance away for a star
 
 int usedStarCount = 512;
 
-const float magnitudeRange = 25;
+const float magnitudeRange = 50;
 
 // the star field - starCount stars represented as x, y and z co-ordinates
 double stars[starCount][4];
@@ -44,7 +44,7 @@ void reset_stars() {
     stars[i][0] = random(-25, 25);
     stars[i][1] = random(-25, 25);
     stars[i][2] = random(0, maxDepth);   // distance
-    stars[i][3] = random(magnitudeRange+1);           // "absolute magnitude"
+    stars[i][3] = 0;           // "absolute magnitude" - Start all zeros so the starfield fades into view.
   }
 }
 
@@ -55,7 +55,10 @@ void doStars() {
 
   // Iterate through the stars reducing the z co-ordinate in order to move the star closer.
   for (int i = 0; i < usedStarCount; i++) {
-    stars[i][2] -= 0.39;
+    stars[i][2] -= 0.39;    // move star closer
+
+    if(stars[i][3] < 49.5)  // increase magnitide unless we are already maxed out.
+      stars[i][3] += 0.39;
 
     // if the star has moved past the screen (z < 0) reposition it far away
     // with random x and y positions.
@@ -63,7 +66,7 @@ void doStars() {
       stars[i][0] = random(-25, 25);
       stars[i][1] = random(-25, 25);
       stars[i][2] = maxDepth;
-      stars[i][3] = random(magnitudeRange+1);
+      stars[i][3] = random(magnitudeRange/3); // start each star rather dim so we can ramp up the madnitude as it get 'closer'
     }
 
     // Convert the 3D coordinates to 2D using perspective projection.
@@ -74,11 +77,15 @@ void doStars() {
     // Draw the star (if it is visible in the screen).
     // Distant stars are smaller than closer stars.
     if ((0 <= x && x < screen_size) && (0 <= y && y < screen_size)) {
+//    if ((0 <= x && x < screen_size) && (0 <= y && y < screen_size) && i == 50) {
       int size = (1 - stars[i][2] / maxDepth) * 8;
 
-      setBrightness(stars[i][3] * 4);
+      setBrightness(stars[i][3] * 2);
       drawACircle(x - origin_x, y - origin_y, (size+1)*3, 6, 13);
     }
+
+//    if(i == 50)  // diagnostic - print one star's stats.
+//      Serial.printf("%f %f %f %f\n", stars[i][0], stars[i][1], stars[i][2], stars[i][3]);
   }
 
   setBrightness(BRIGHTNESS_DEFAULT);
